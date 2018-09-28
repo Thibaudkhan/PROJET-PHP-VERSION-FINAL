f3821343003d84e357b86c58ecdf476bddbf0e6d
Thibaud <thibaudr1999@hotmail.com>
2018-09-28 03:21:22 +0200

    Liens marche
    Correction petit bugg

--- lien/PontoiseChamperet.php
+++ lien/PontoiseChamperet.php
@@ -1,12 +1,12 @@
 <?php
 
-require '../ThibaudBaseTEST.php';
+require '../ThibaudBase.php';
 
      $db = Database::connect();
 
     $statement = $db->query('SELECT nom, sPontoise, sChamperet FROM personnes WHERE sPontoise= 1 AND sChamperet = 1');
 
-    echo '<h1>Champeret et Pontoise </1>';
+    echo '<h1>Champeret et Pontoise </h1>';
 
     while($test = $statement->fetch()){
     	?>
